1fe4963e01a3353f79dc0ed8e2d5e4e9a1847250
BinotaLIU <binota@binota.org>
2017-01-10 18:56:42 +0800

    (opt): add a random number for ecpay

---
+++ app/Http/Controllers/Procurement/Ticket/JapanShipmentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Procurement\Ticket;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class JapanShipmentController extends Controller
+{
+    //
+}
