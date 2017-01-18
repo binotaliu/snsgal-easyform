3408afd405e91c5d726629cbfbb2a0ae71deb44c
BinotaLIU <binota@binota.org>
2017-01-18 19:16:36 +0800

    (add): artisan command configs:set

---
+++ app/Console/Commands/SetConfig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Repositories\ConfigRepository;
+use Illuminate\Console\Command;
+
+class SetConfig extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'configs:set {key} {value}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set config';
+
+    /**
+     * @var ConfigRepository
+     */
+    protected $configRepository;
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct(ConfigRepository $configRepository)
+    {
+        parent::__construct();
+
+        $this->configRepository = $configRepository;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->configRepository->updateConfig($this->argument('key'), $this->argument('value'));
+    }
+}
