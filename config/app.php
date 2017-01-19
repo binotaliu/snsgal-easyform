c9b90e27452e003fe03d9b46fb01d97072091907
BinotaLIU <binota@binota.org>
2017-01-19 19:04:02 +0800

    (add): set application name in .env file

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => '穹ノ空',
+    'name' => env('APP_NAME', '穹ノ空'),
 
     /*
     |--------------------------------------------------------------------------
